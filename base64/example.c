b8627c47aec583598e8e72b7bf06a85afaeb4d71
Egor Lord <egor.lord-127@yandex.com>
2017-08-22 10:05:22 -0700

    new method

---
+++ base64/example.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+static const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+int base64_encode(unsigned char* in,  char* out, unsigned int in_len)
+{
+    int length = 0;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+
+    while (in_len--) {
+        char_array_3[i++] = *(in++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for(i = 0; (i <4) ; i++)
+            {
+                length++;
+                *out++ = base64_chars[char_array_4[i]];
+            }
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for(j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; (j < i + 1); j++)
+        {
+            length++;
+            *out++ = base64_chars[char_array_4[j]];
+        }
+        while((i++ < 3))
+        {
+            length++;
+            *out++ = '=';
+        }
+    }
+
+    return length;
+}
+
+void main()
+{
+    char in[] = "abcdefg";
+    char out[1024] = "";
+    int len = base64_encode((unsigned char*)in, out, sizeof(in) - 1);
+    printf("%s:%d", out, len);
+}
